b3a66086c24e5f226f60c1e4bd4e4d89b5153763
EmmanuelAdu <christorenzo20@gmail.com>
2024-01-14 07:38:13 +0000

    Return number of objects for each type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Create a route `/status` on the object app_views.
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,3 +16,19 @@ def status():
     """
     response = {'status': 'OK'}
     return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrieves the number of each objects by types.
+    """
+    stats = {
+      'amenities': storage.count('Amenity'),
+      'cities': storage.count('City'),
+      'places': storage.count('Place'),
+      'reviews': storage.count('Review'),
+      'states': storage.count('State'),
+      'users': storage.count('User')
+    }
+    return jsonify(stats)
